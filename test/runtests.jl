0b85cbe76fd2a365da654cc6e3f0bb79d132a5bf
C Huang <chen1huang2@gmail.com>
2019-12-18 21:20:34 +0000

    clean slate

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,14 @@
-# File test/runtests.jl
 using Graphene
 using Test
 
 function tests()
     @testset "Subset of tests" begin
-        include("test_id_generator.jl")
+        # include("test_id_generator.jl")
         include("test_fileio.jl")
-        include("test_atoms.jl")
+        # include("test_atoms.jl")
         # include("test_bonds.jl")
         # include("test_polygons.jl")
-        include("test_plotting.jl")
+        # include("test_plotting.jl")
   end
 end
 
